06385289d44f522a52cf74f74a465ac975e61ded
honzikfischer@outlook.cz <honzikfischer@outlook.cz>
2019-04-08 15:14:49 +0200

    Interface přidán

---
+++ Broucy/Akce.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broucy
+{
+    abstract class Akce
+    {
+        abstract public void UpravBroucka(Brouk brouk);
+    }
+    class OtocDoprava : Akce
+    {
+        public override void UpravBroucka(Brouk brouk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class OtocDoleva : Akce
+    {
+        public override void UpravBroucka(Brouk brouk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Jdi : Akce
+    {
+        public override void UpravBroucka(Brouk brouk)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Broucy/Class1.cs
+++ Broucy/Class1.cs
@@ -5,11 +5,44 @@ using System.Text;
 
 namespace Broucy
 {
-    public class Class1
+    public class Brouk
+    {
+        public Brouk(Mapa mapa,Souradnice souradnice, Otoceni otoceni)
+        {
+            this.mapa = mapa;
+            this.souradnice = souradnice;
+            this.otoceni = otoceni;
+        }
+        internal Mapa mapa;
+        public Souradnice souradnice { get; internal set; }
+        public enum Otoceni {nahoru,dolu,doprava,doleva}
+        public Otoceni otoceni { get; internal set; }
+        internal int stav;
+
+        public void UdelejTah()
+        {
+
+        }
+    }
+    public struct Souradnice
+    {
+        int x, y;
+    }
+    class StavyAKroky
+    {
+        public Prikaz VratPrikaz(int Stav)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    struct Prikaz
+    {
+        Akce Kytka, Prazdno;
+        int NovyStavKytka, NovyStavPrazdno;
+    }
+    
+    public class Mapa
     {
 
-
-
-        //ty kurwo
     }
 }
